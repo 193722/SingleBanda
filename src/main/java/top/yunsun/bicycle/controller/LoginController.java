8d797531fca66b693aec09c1647424481f735407
zheye0722@qq.com <zheye0722@qq.com>
2017-12-07 23:11:37 +0800

    整合redis

--- src/main/java/top/yunsun/bicycle/controller/LoginController.java
+++ src/main/java/top/yunsun/bicycle/controller/LoginController.java
@@ -2,10 +2,15 @@ package top.yunsun.bicycle.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.servlet.ModelAndView;
+import top.yunsun.bicycle.core.redis.RedisUtils;
 
 
 @RestController
@@ -13,15 +18,20 @@ public class LoginController {
 
     private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
+    @Autowired
+    private RedisUtils redisUtils;
+
     @RequestMapping("/index.html")
     public ModelAndView login(ModelMap modelMap) {
         logger.info("跳转首页");
+        redisUtils.set("mykey","这是我的redis使用测试");
         return new ModelAndView("index");
     }
 
     @RequestMapping("/list.html")
     public String list(ModelMap modelMap) {
         logger.info("跳转集合页面");
+        logger.info("redis得到的mykey的value：{}",redisUtils.get("mykey"));
         return "404";
     }
 
